3bc35bb719d63158ae33fc0af2a14a22f32a4e61
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-07-30 01:23:50 +0600

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,15 +10,15 @@ const getById = async (req, res) => {
   const { contactId } = req.params;
   const contact = await contacts.findById(contactId);
 
-  if (contact) {
-    res.status(200).json(contact);
-  } else {
-    res.status(404).json({ message: "Contact not found" });
+  if (!contact) {
+    return res.status(404).json({ message: "Contact not found" });
   }
+
+  res.status(200).json(contact);
 };
 
 const add = async (req, res) => {
-  const result = await contacts.create(req.body)
+  const result = await contacts.create(req.body);
   res.status(201).json(result);
 };
 
@@ -26,34 +26,35 @@ const removeById = async (req, res) => {
   const { contactId } = req.params;
   const updateContacts = await contacts.findByIdAndRemove(contactId);
 
-  if (updateContacts) {
-    res.status(200).json({ message: "Contact deleted" });
-  } else {
-    res.status(404).json({ message: "Not found" });
+  if (!updateContacts) {
+    return res.status(404).json({ message: "Not found" });
   }
+  res.status(200).json({ message: "Contact deleted" });
 };
 
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const updatedContact = await contacts.findByIdAndUpdate(contactId, req.body, {new: true});
+  const updatedContact = await contacts.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
 
-  if (updatedContact) {
-    res.status(200).json(updatedContact);
-  } else {
+  if (!updatedContact) {
     res.status(404).json({ message: "Not found" });
   }
+  res.status(200).json(updatedContact);
 };
 
 const updateStatusContact = async (req, res) => {
   const { contactId } = req.params;
-  const updatedContact = await contacts.findByIdAndUpdate(contactId, req.body, {new: true});
+  const updatedContact = await contacts.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
 
-  if (updatedContact) {
-    res.status(200).json(updatedContact);
-  } else {
-    res.status(404).json({ message: "Not found" });
+  if (!updatedContact) {
+    return res.status(404).json({ message: "Not found" });
   }
-}
+  res.status(200).json(updatedContact);
+};
 
 module.exports = {
   getAll,
